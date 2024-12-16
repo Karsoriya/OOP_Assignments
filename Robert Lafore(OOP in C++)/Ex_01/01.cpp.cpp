d361bf01bb588fab38a2b1004eab0ac27f89908b
Karsoriya <soriyabasharat@gmail.com>
2024-12-16 12:48:58 +0530

    Update 01.cpp

--- Robert Lafore(OOP in C++)/Ex_01/01.cpp.cpp
+++ Robert Lafore(OOP in C++)/Ex_01/01.cpp.cpp
@@ -8,8 +8,8 @@ int main() {
     cout << "Enter a positive integer : ";
     cin >> number;
 
-    for (i = 1; i <= 20; ++i) {
-        for ( j = 1; j <= 10; ++j) {
+    for (i = 1; i <= 20; i++) {
+        for ( j = 1; j <= 10; j++) {
             cout  << number * ((i - 1) * 10 + j)<<"\t";
            
         }
